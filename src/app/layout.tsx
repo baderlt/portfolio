1b834dca5ee10ab2863a7d2510c4350e4a45f167
baderlt <baderlatrache10@gmail.com>
2024-08-05 16:49:20 +0100

    fix error prod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.variable}`} id="body">
-        <Main children={children} />
+        <Main  >
+          {children}
+          </Main>
       </body>
     </html>
   );
